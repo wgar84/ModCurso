7458ba711885ce48fcbe25504d0d555aaff4e83d
Guilherme Garcia <wgar@usp.br>
2017-05-06 15:23:53 -0300

    started tutorial on linear algebra

--- Aulas/aux.R
+++ Aulas/aux.R
@@ -5,6 +5,8 @@ render('02/index.Rmd', output_dir = '02/')
 
 render('02/guia.Rmd', output_dir = '02/')
 
+render('../Tutoriais/algelin.Rmd', output_dir = '../Tutoriais')
+
 browseURL('02/index.html')
 
 system('evince 02/guia.pdf &')
