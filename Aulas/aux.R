97d2c8ae14fdacc9dac53ac8df0a343a1d10ebdd
Guilherme Garcia <wgar@usp.br>
2017-05-07 22:25:38 -0300

    minor

--- Aulas/aux.R
+++ Aulas/aux.R
@@ -11,8 +11,6 @@ render('../Tutoriais/matrizes.Rmd', output_dir = '../Tutoriais')
 
 render('../Tutoriais/inicio.Rmd', output_dir = '../Tutoriais')
 
-
-
 browseURL('02/index.html')
 
 browseURL('../Tutoriais/algelin.html')
