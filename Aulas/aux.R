949431649ed84ac92a9c3bc959f8f98024db5e8c
Guilherme Garcia <wgar@usp.br>
2017-05-08 21:38:28 -0300

    minor

--- Aulas/aux.R
+++ Aulas/aux.R
@@ -5,11 +5,14 @@ render('02/index.Rmd', output_dir = '02/')
 
 render('02/guia.Rmd', output_dir = '02/')
 
-render('../Tutoriais/algelin.Rmd', output_dir = '../Tutoriais')
+render('../Tutoriais/algelin.Rmd', output_dir = '../Tutoriais', output_format = 'pdf_document')
+
+render('../Tutoriais/algelin.Rmd', output_dir = '../Tutoriais', output_format = 'html_document')
 
 render('../Tutoriais/matrizes.Rmd', output_dir = '../Tutoriais')
 
 render('../Tutoriais/inicio.Rmd', output_dir = '../Tutoriais')
+
 
 render('../index.Rmd', output_dir = '../')
 
@@ -19,3 +22,5 @@ browseURL('02/index.html')
 browseURL('../Tutoriais/algelin.html')
 
 system('evince 02/guia.pdf &')
+
+system('evince ../Tutoriais/algelin.pdf &')
